3e025b3dc8b49586ea373c4df720b11a711e0afe
nesli9 <doanneslihan@gmail.com>
2022-10-25 20:12:29 +0300

    Classes

---
+++ CSharpCourse/Classes/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Customer
+    {
+        //field (alan) tanımlamak
+        //public string FirstName;
+        public int Id { get; set; } 
+        
+        //properties tanımlamak. (class özelliklerini tanımlayacaksak bu kalıp kullanılmalı)
+        private string _firstName; //field tanımlanır
+        public string FirstName 
+        {
+            
+            get 
+            { 
+                return "Mrs." + _firstName;
+            }
+            set 
+            { 
+                _firstName = value;
+            } 
+        }
+        public string LastName { get; set; }
+        public string city { get; set; }
+
+    }
+}
---
+++ CSharpCourse/Classes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager manager = new CustomerManager(); //classın referansı (örneği) oluşturulmuş oldu.
+            manager.Add();
+            manager.Update();
+        
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+            Customer customer =  new Customer();
+            customer.city = "ankara";
+            customer.Id = 1;
+            customer.FirstName = "engin";
+            customer.LastName = "demiroğ";
+
+            Customer customer2 = new Customer
+            {
+                Id = 2,
+                city = "istanbul",
+                FirstName = "derin",
+                LastName = "demiroğ"
+            };
+
+            Console.WriteLine(customer2.FirstName);
+
+            Console.ReadLine();
+        
+        }
+
+    }
+
+    
+
+    
+
+}
